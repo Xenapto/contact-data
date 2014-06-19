5227ce1d21a79e7d29e7cc97c86384efd21afac0
Dominic Sayers <dominic@sayers.cc>
2014-06-19 13:07:06 +0100

    Add search and info methods to Link

--- lib/contact-data/link.rb
+++ lib/contact-data/link.rb
@@ -5,12 +5,25 @@ module ContactData
 
     class << self
       def latest(params = {})
+        Fetcher.get :latest, options_from(params)
+      end
+
+      def info(params = {})
+        Fetcher.get :info, options_from(params)
+      end
+
+      def search(params = {})
+        Fetcher.get :search, options_from(params)
+      end
+
+      private
+
+      def options_from(params = {})
         options = { base: BASE }
 
         options[:verbose] = params.delete(:verbose) if params.key? :verbose
         options[:params]  = params unless params.empty?
-
-        Fetcher.get(:latest, options)
+        options
       end
     end
   end
--- spec/contact-data_link_spec.rb
+++ spec/contact-data_link_spec.rb
@@ -3,10 +3,26 @@ require 'spec_helper'
 require 'contact-data'
 
 describe ContactData::Link do
+  let(:url) { 'http://iana.org' }
+
   it 'gets latest links with related slug' do
     VCR.use_cassette('links_latest') do
       result = ContactData::Link.latest verbose: true
       expect(result).to be_an(Array)
+    end
+  end
+
+  it 'gets metadata about a url' do
+    VCR.use_cassette('links_info_search') do
+      result = ContactData::Link.info url: url, verbose: true
+      expect(result).to be_a(Hash)
+    end
+  end
+
+  it 'gets metadata & contact from a url' do
+    VCR.use_cassette('links_info_search') do
+      result = ContactData::Link.info url: url, verbose: true
+      expect(result).to be_a(Hash)
     end
   end
 end
